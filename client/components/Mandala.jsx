ceabfb78fb252b2209824e8e239518109b05161c
Melissa Mepham <m.a.mepham@gmail.com>
2018-05-01 21:53:23 +1200

    animating an SVG circle in React

--- client/components/Mandala.jsx
+++ client/components/Mandala.jsx
@@ -4,11 +4,34 @@ import { render } from 'react-dom'
 
 import {HashRouter as Router, Route} from 'react-router-dom'
 
-const Mandala = (props) => {
-  return (
-    <div>Hello World</div>
+class Mandala extends React.Component {
+  constructor(props) {
+  super(props)
+  this.state = {
+    }
+  }
 
-  )
+  componentDidMount() {
+  }
+
+
+  render() {
+    return (
+      <div>
+        <svg className="background box" height="400" width="400">
+          <circle cx="200" cy="200" r="30" fill="#FFD080" opacity="0.5">
+            <animate
+              attributeType="XML"
+              attributeName="r"
+              dur="5s"
+              values="0; 50; 0"
+              repeatCount="indefinite" />
+          </circle>
+        </svg>
+      </div>
+
+    )
+  }
 }
 
   export default Mandala
